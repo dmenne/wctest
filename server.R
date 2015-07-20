a92bc9923563562144dfcecbb32618df01f382c3
Dieter Menne <dieter.menne@menne-biomed.de>
2015-07-20 16:41:23 +0200

    Improved show/hide

--- server.R
+++ server.R
@@ -1,24 +1,33 @@
 library(pairwiseCI)
 library(DT)
 shinyServer(function(input, output, session) {
-  d = NULL
-  pc = NULL
-  output$results = renderText({
-    input$computeButton
-    #    if (input$computeButton == 0)
-#      return(NULL)
+  hide("results")
+
+  pc = reactive({
+    if (input$computeButton == 0)
+      return(NULL)
     data = isolate(input$data)
     if (nchar(data) < 5) return(NULL)
-    d <<- na.omit(read.table(textConnection(data), sep = "\t", header = TRUE))
-    d[,1] <<- as.factor(d[,1])
+    d = na.omit(read.table(textConnection(data), sep = "\t", header = TRUE))
+    d[,1] = as.factor(d[,1])
 
     form = as.formula(paste0(names(d)[2],"~",names(d)[1]))
-    pc <<- pairwiseCI(form, d, method = "Param.diff")
-    paste(nrow(as.data.frame(d)), " vollständige Werte.")
+    p = pairwiseCI(form, d, method = "Param.diff")
+    attr(p, "nrow") = nrow(d)
+    p
   })
+
+  output$summary = renderText({
+    if (is.null(pc())) return(NULL)
+    show("summary")
+    paste(attr(pc(),"nrow"), "valid values")
+   }
+  )
   observe({
     if (input$clearButton == 0)
       return(NULL)
+    hide("results")
+    hide("summary")
     updateAceEditor(session, "data",value = "\n")
   })
 
@@ -27,18 +36,18 @@ shinyServer(function(input, output, session) {
   }, deleteFile = FALSE)
 
   output$diffplot = renderPlot({
-    input$computeButton
-    if (is.null(pc)) return(NULL)
-    par(oma = c(6,0,0,0))
-    plot(pc, main = "95% Konfidenzintervalle")
-    mtext("Wenn einer der Balken den Wert 0 NICHT kreuzt, ist die Differenz\nsignifikant von Null verschieden mit p < 0.05.\nWenn alle Balken den Wert 0 kreuzen, kann ein signifikanter Effekt nicht festgestellt werden.\nDie geliebten p-Werte werden hier bewusst nicht ausgegeben,\nda die Konfidenzintervalle aussagekräftiger sind.",1, adj = 0, padj = 2)
+    p = pc()
+    if (is.null(p)) return(NULL)
+    ylim = c(0, length(p$byout[[1]][[1]]) + 1)
+    show("results")
+
+    plotCI(p, main = "95% Konfidenzintervalle", HL = TRUE, lines = 0, ylim = ylim)
   })
 
-    output$table = DT::renderDataTable({
-      input$computeButton
-      if (is.null(pc)) return(NULL)
-      as.data.frame(pc)
-    },
-    options = list(autoWidth = TRUE, dom = 't'))
+
+  output$table = DT::renderDataTable({
+    if (is.null(pc())) return(NULL)
+    as.data.frame(pc())},
+      options = list(autoWidth = TRUE, dom = 't'))
 })
 
--- ui.R
+++ ui.R
@@ -1,19 +1,25 @@
 library(shinyAce)
+library(shinyjs)
 
 shinyUI(fluidPage(
+  useShinyjs(),
   titlePanel("Ungepaarter Wilcoxon-Test (Mann-Whitney U-Test) nach Gruppen"),
   sidebarLayout(
     sidebarPanel( "",
       actionButton("computeButton","Berechnen"),
       actionButton("clearButton","Löschen"),
-      textOutput("results"),
+      textOutput("summary"),
       imageOutput("helpImage")
     ),
     mainPanel("Daten: Zweispaltige Excel-Daten aus der Zwischenablage mit STRG-V hier eingeben",
       aceEditor("data", "", mode = "plain_text"),
-      tags$script(type = "text/javascript", HTML("ace.edit('data').setOptions({tabSize:12});")),
-      plotOutput("diffplot"),
-      DT::dataTableOutput('table')
+      wellPanel(id = "results",
+        tags$script(type = "text/javascript", HTML("ace.edit('data').setOptions({tabSize:12});")),
+        plotOutput("diffplot"),
+        HTML("<ul><li>Wenn eine der Konfidenzbalken den Wert 0 NICHT kreuzt, ist diese Differenz signifikant von Null verschieden mit p<0.05.</li><li>Wenn alle Balken den Wert 0 kreuzen, wurde kein signifikanter Effekt gefunden. Das heißt aber nicht, dass keine Unterschied vorhanden ist, er ist nur mit diesen Daten nicht nachweisbar.</li><li>p-Werte werden hier bewusst nicht ausgegeben, da die Konfidenzintervalle aussagekräftiger sind.</li></ul>"),
+        DT::dataTableOutput('table')
+      )
+
     )
   )
 ))

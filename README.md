b74ac561b13b664e5ebf2e3f698c336a0f7f76f0
Dieter Menne <dieter.menne@menne-biomed.de>
2015-11-02 12:01:19 +0100

    Corrected readme Added source on github link

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
-# Wilcoxon from Excel
-German Shiny page to compute pairwise Wilcoxon test from data marked with group labels.
+# Wilcoxon-Test from Excel data
 
-Allows for copy/paste of data from Excel.
+German Shiny page to compute pairwise Wilcoxon tests from data marked with group labels.
+Allows for copy/paste of data from Excel. Creates plots of confidence intervals of pairwise differences, and box-whiskers plot of the raw data, with and with outlier suppression. By design, no p-values are output, only confidence intervals of the differences. 
 
-Creates plots of confidence intervals of pairwise differences, and box-whiskers plot of the raw data, with and with outlier suppression.
+You can create sample data to simulate multiple repeats of a study and observe the horribly different conclusions that could be drawn on multiple runs. 
 
-By design, no p-values are output, only confidence intervals of the differences. 
-
-You can create sample date to simulate an experiment and observe the horribly different conclusions reached when an experiment is repeated. 
-
-# Demonstration
+## Demonstration
 
 https://menne-biomed.shinyapps.io/wctests
 
-# Screenshot
+## Screenshot
 
 ![Screenshot](screenshot.png)
--- ui.R
+++ ui.R
@@ -9,7 +9,8 @@ shinyUI(fluidPage(
     sidebarPanel( "",
       textOutput("summary"),
       imageOutput("helpImage", height = "430px"),
-      HTML("<ul><li>Kopieren Sie die Spalten wie oben in eine extra Seite von Excel.</li><li>In der ersten Spalte steht der Gruppenname, am besten als Textkürzel; in der zweiten die gemessenen Werten.</li><li>Spaltenüberschriften müssen vorhanden sein.</li><li>Nehmen Sie die Daten mit Spaltenüberschriften in die Zwischenablage (Strg-C)</li><li>Pasten Sie die Daten in das Feld oben rechts (Strg-V).</li><li>Wenn nur eine Gruppe vorhanden ist, wird ein Test der Gruppe gegen Null ausgegeben. Wenn die Messwerte in diesem Fall paarweise Differenzen sind, können Sie so den gepaarten Wilcoxon Test berechnen.</li></ul>")
+      HTML("<ul><li>Kopieren Sie die Spalten wie oben in eine extra Seite von Excel.</li><li>In der ersten Spalte steht der Gruppenname, am besten als Textkürzel; in der zweiten die gemessenen Werten.</li><li>Spaltenüberschriften müssen vorhanden sein.</li><li>Nehmen Sie die Daten mit Spaltenüberschriften in die Zwischenablage (Strg-C)</li><li>Pasten Sie die Daten in das Feld oben rechts (Strg-V).</li><li>Wenn nur eine Gruppe vorhanden ist, wird ein Test der Gruppe gegen Null ausgegeben. Wenn die Messwerte in diesem Fall paarweise Differenzen sind, können Sie so den gepaarten Wilcoxon Test berechnen.</li></ul>"),
+    helpText(a("Source on github", href = "https://github.com/dmenne/wctest", target = "_blank"))
     ),
     mainPanel("Daten: Zweispaltige Excel-Daten aus der Zwischenablage mit STRG-V hier eingeben",
       aceEditor("data", "", mode = "plain_text"),
